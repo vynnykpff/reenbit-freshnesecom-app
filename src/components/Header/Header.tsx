79e187abd43d97fca5fa11c6411956433b48fe0d
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-10-19 13:56:05 +0300

    fix(S02): replace magic number

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,10 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 import { ROUTES } from "@/common/constants";
 
+const MAX_RANGE = 1000;
+
 export const Header: FC = () => {
-  const tempProductId = Math.floor(Math.random() * 1000);
+  const tempProductId = Math.floor(Math.random() * MAX_RANGE);
   return (
     <header>
       <ul>
