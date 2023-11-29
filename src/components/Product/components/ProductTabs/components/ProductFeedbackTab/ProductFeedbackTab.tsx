170f940d9b1534938249fc9c559e3068a8b69f04
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-11-29 12:40:07 +0200

    fix(S11): typing errors

--- src/components/Product/components/ProductTabs/components/ProductFeedbackTab/ProductFeedbackTab.tsx
+++ src/components/Product/components/ProductTabs/components/ProductFeedbackTab/ProductFeedbackTab.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { motion } from "framer-motion";
 import { Product } from "@/common/types";
 import { NoMatches } from "@/components/UI";
-import { AnimationDefaultDuration, ProductTabsVariants, animationDefaultVariants } from "@/common/constants";
+import { AnimationDefaultDuration, ProductRating, ProductTabsVariants, animationDefaultVariants } from "@/common/constants";
 import commonStyles from "@/styles/Common.module.scss";
 import { getAnimationVariant } from "@/utils";
 import { ProductFeedback } from "./components";
@@ -21,7 +21,9 @@ export const ProductFeedbackTab: FC<Props> = ({ selectedTab, ...props }) => {
       className={commonStyles.productTabsContentContainer}
     >
       {feedbackData.length ? (
-        feedbackData.map(credentials => <ProductFeedback key={credentials.id} isRating={isRating} {...credentials} />)
+        feedbackData.map(credentials => (
+          <ProductFeedback rating={ProductRating.MIN} key={credentials.id} isRating={isRating} {...credentials} />
+        ))
       ) : (
         <NoMatches />
       )}
